98741b97536210f49c72517f1f1cec076dfd1223
Joshua Darnell <josh@kurotek.com>
2020-03-18 17:46:25 -0700

    rewrote to remove deprecated components

--- src/main/java/org/reso/auth/TokenHttpClientFactory.java
+++ src/main/java/org/reso/auth/TokenHttpClientFactory.java
@@ -1,50 +1,58 @@
 package org.reso.auth;
 
+import org.apache.http.Header;
 import org.apache.http.client.HttpClient;
-import org.apache.http.protocol.HTTP;
-import org.apache.logging.log4j.Logger;
+import org.apache.http.conn.HttpClientConnectionManager;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.conn.BasicHttpClientConnectionManager;
+import org.apache.http.message.BasicHeader;
 import org.apache.logging.log4j.LogManager;
-import org.apache.olingo.client.core.http.DefaultHttpClientFactory;
+import org.apache.logging.log4j.Logger;
+import org.apache.olingo.client.core.http.AbstractHttpClientFactory;
 import org.apache.olingo.commons.api.http.HttpMethod;
 
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
- * Extends DefaultHttpClientFactory with one that can accept tokens passed in to make requests.
+ * Extends AbstractHttpClientFactory with one that can accept tokens passed in to make requests.
  */
-public class TokenHttpClientFactory extends DefaultHttpClientFactory {
+public class TokenHttpClientFactory extends AbstractHttpClientFactory {
   String token;
 
   private static final Logger LOG = LogManager.getLogger(TokenHttpClientFactory.class);
 
+  HttpClientConnectionManager connectionManager = null;
+
   /**
    * Constructor for use with tokens.
-   *
    * @param token the token to be used for server requests.
    */
   public TokenHttpClientFactory(String token) {
     this.token = token;
   }
 
-  @SuppressWarnings("deprecation")
   @Override
-  public org.apache.http.impl.client.DefaultHttpClient create(final HttpMethod method, final URI uri) {
-    final org.apache.http.impl.client.DefaultHttpClient client = new org.apache.http.impl.client.DefaultHttpClient();
+  public HttpClient create(final HttpMethod method, final URI uri) {
+    BasicHeader authHeader = new BasicHeader("Authorization", "Bearer " + token);
+    List<Header> headers = new ArrayList<>();
+    headers.add(authHeader);
 
-    client.getParams().setParameter(HTTP.USER_AGENT, USER_AGENT);
+    connectionManager = new BasicHttpClientConnectionManager();
 
-    // add auth token using the vendor's bearer token passed from the command line.
-    // TODO: handle refresh tokens
-    client.addRequestInterceptor((request, context) -> request.addHeader("Authorization", "Bearer " + token));
-
-    return client;
+    return HttpClientBuilder.create()
+        .setUserAgent(USER_AGENT)
+        .setDefaultHeaders(headers)
+        .setConnectionManager(connectionManager)
+        .build();
   }
 
-  @SuppressWarnings("deprecation")
+
   @Override
   public void close(final HttpClient httpClient) {
     try {
-      httpClient.getConnectionManager().shutdown();
+      connectionManager.shutdown();
     } catch (Exception ex) {
       LOG.error(ex.toString());
     }
