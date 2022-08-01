f1452b5541dce0ca0db3687dbb90fadfa056f482
Joshua Darnell <josh@kurotek.com>
2022-08-01 09:25:49 -0700

    #129: Added test command

--- runCommander.sh
+++ runCommander.sh
@@ -1,50 +1,71 @@
 #!/bin/bash
+echo "Job: RESO Data Dictionary Testing"
 echo "Started at: " date
 echo "Args: $*"
 env
-echo "Running Commander..."
-echo "certificationRequestId: $1"
 
-certificationPath="/certification/$1"
-
-if [ -n "$1" ]; then
-  echo ""
-
-  echo "Checking for config file: /certification/$1/config.xml"
-  ls "/certification/$1/config.xml"
-  echo ""
-
-  echo "Changing to Commander Directory"
-  cd "/web-api-commander" || exit
-
-  echo "Running Metadata Tests. Command: gradle -DpathToRESOScript=$certificationPath/config.xml"
-  gradle testDataDictionary_1_7 "-DpathToRESOScript=$certificationPath/config.xml" > "$certificationPath/data-dictionary.log"
-
-  status=$?
-  if [ $status -eq 1 ]; then
-    echo "Data Dictionary testing failed for certificationRequestId: $1"
-  else
-    echo "Data Dictionary testing succeeded for certificationRequestId: $1"
-  fi
-  echo ""
-
-  echo "Running Data Availability Tests. Command: gradle testDataAvailability_1_7 -DpathToRESOScript=$certificationPath/config.xml"
-  gradle testDataAvailability_1_7 "-DpathToRESOScript=$certificationPath/config.xml" > "$certificationPath/data-availability.log"
-
-  status=$?
-  if [ $status -eq 1 ]; then
-    echo "Data Availability testing failed for certificationRequestId: $1"
-  else
-    echo "Data Availability testing succeeded for certificationRequestId: $1"
-  fi
-  echo ""
-
-  echo "Copying files: cp -R commander.log build/certification $certificationPath"
-  cp -R "commander.log" "build/certification" "$certificationPath"
-  ls -alh "$certificationPath"
-  echo "Copying complete!"
-
-else
-  echo "ERROR: certificationRequestId parameter missing from args!"
-  return 1
+if [ -z "$1" ]; then
+  echo "ERROR: 'certificationRequestId' parameter missing from args!"
+  exit 1
 fi
+
+if [ -z "$2" ]; then
+  echo "ERROR: 'certificationCommand' parameter missing from args!"
+  exit 1
+fi
+
+certificationCommand="$1"
+certificationRequestId="$2"
+certificationPath="/certification/$certificationRequestId"
+
+echo "Running Commander..."
+echo "certificationCommand: $certificationCommand"
+echo "certificationRequestId: $certificationRequestId"
+echo "certificationPath: $certificationPath"
+
+echo "Checking for config file: $certificationPath/config.xml"
+ls "$certificationPath/config.xml"
+
+status=$?
+if [ $status -eq 1 ]; then
+  echo "ERROR: Could not find config file!"
+  exit 1
+fi
+
+echo "Changing to Commander Directory"
+cd "/web-api-commander" || exit 1
+
+echo "Running Tests! Command: gradle $certificationCommand -DpathToRESOScript=$certificationPath/config.xml"
+gradle "$certificationCommand" "-DpathToRESOScript=$certificationPath/config.xml" #> "$certificationPath/$certificationCommand.log"
+
+status=$?
+if [ $status -eq 1 ]; then
+  echo "ERROR: Command '$certificationCommand' failed for certificationRequestId: $certificationRequestId"
+  exit 1
+else
+  echo "SUCCESS: Command '$certificationCommand' succeeded for certificationRequestId: $certificationRequestId"
+fi
+
+echo "Copying files: cp -R commander.log build/certification $certificationPath"
+cp -R "commander.log" "build/certification" "$certificationPath"
+
+status=$?
+if [ $status -eq 1 ]; then
+  echo "ERROR: Could not copy files to '$certificationPath'!"
+  exit 1
+else
+  echo "Files copied!"
+fi
+
+ls -alh "$certificationPath"
+status=$?
+if [ $status -eq 1 ]; then
+  echo "ERROR: Could not list files in '$certificationPath'!"
+  exit 1
+else
+  echo "Files copied!"
+fi
+
+echo "Testing complete!"
+
+
