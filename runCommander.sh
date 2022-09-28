abc27aa9dfbe3dfbb5a5348398c4a2662b6b3368
Joshua Darnell <darnjo@users.noreply.github.com>
2022-09-28 10:00:43 -0700

    Issue #129: Create file copy gradle tasks for automated testing (#136)
    * Issue #129: Added Temp Configs for Docker Image
    
    * #129: Updated GradleDockerfile
    
    * #129: Added build scripts and Docker file for automated builds
    
    * #129: Updated Docker container and resoCommander.sh
    
    * #129: Added test command
    
    * #129: Added gradle.properties with updated memory max
    
    * #129: Added preparation step to build.gradle
    
    * #129: Added automated testing container handling

---
+++ runCommander.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+echo "Job: RESO Data Dictionary Testing"
+echo "Started at: " date
+echo "Args: $*"
+env
+
+if [ -z "$1" ]; then
+  echo "ERROR: 'certificationRequestId' parameter missing from args!"
+  exit 1
+fi
+
+if [ -z "$2" ]; then
+  echo "ERROR: 'certificationCommand' parameter missing from args!"
+  exit 1
+fi
+
+certificationCommand="$1"
+certificationRequestId="$2"
+certificationPath="/certification/$certificationRequestId"
+
+echo "Running Commander..."
+echo "certificationCommand: $certificationCommand"
+echo "certificationRequestId: $certificationRequestId"
+echo "certificationPath: $certificationPath"
+
+echo "Checking for config file: $certificationPath/config.xml"
+ls "$certificationPath/config.xml"
+
+status=$?
+if [ $status -eq 1 ]; then
+  echo "ERROR: Could not find config file!"
+  exit 1
+fi
+
+echo "Changing to Commander Directory"
+cd "/web-api-commander" || exit 1
+
+echo "Running Tests! Command: gradle $certificationCommand -DpathToRESOScript=$certificationPath/config.xml"
+gradle "$certificationCommand" "-DpathToRESOScript=$certificationPath/config.xml" > "$certificationPath/$certificationCommand.log"
+
+status=$?
+if [ $status -eq 1 ]; then
+  echo "ERROR: Command '$certificationCommand' failed for certificationRequestId: $certificationRequestId"
+  exit 1
+else
+  echo "SUCCESS: Command '$certificationCommand' succeeded for certificationRequestId: $certificationRequestId"
+fi
+
+echo "Copying files: cp -R commander.log build/certification $certificationPath"
+cp -R "commander.log" "build/certification" "$certificationPath"
+
+status=$?
+if [ $status -eq 1 ]; then
+  echo "ERROR: Could not copy files to '$certificationPath'!"
+  exit 1
+else
+  echo "Files copied!"
+fi
+
+ls -alh "$certificationPath"
+status=$?
+if [ $status -eq 1 ]; then
+  echo "ERROR: Could not list files in '$certificationPath'!"
+  exit 1
+else
+  echo "Files copied!"
+fi
+
+echo "Testing complete!"
+
+
